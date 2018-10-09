fbb59d4b58ac63fc0173895f67d35b692ec1d3ed
Ali Moreno <infrastructure@xenotrue.com>
2018-10-09 18:22:11 -0400

    adding comments

--- xtshell.sh
+++ xtshell.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 # by Ali Moreno <http://alimoreno.info>
 
+# Window 0 "Syslog" / Pane 0
 tmux new-session -s 'XT Shell' -n Syslog \; \
   send-keys 'sudo multitail /var/log/syslog' C-m \; \
+  # Window 1 "System" / Pane 0
   new-window -n System \; \
   send-keys 'htop' C-m \; \
+  # Window 1 "System" / Pane 1
   split-window -v \; \
   send-keys 'watch -n 1 df -H' C-m \; \
+  # Window 1 "System" / Pane 3
   split-window -h \; \
   send-keys 'nload eth0' C-m \; \
+  # Window 1 "System" / Pane 2
   select-pane -t 1 \; \
   split-window -v \; \
+  # Window 2 "Dev" / Pane 0
   new-window -n Dev \; \
+  # Window 2 "Dev" / Pane 2
   split-window -h -p 50 \; \
   send-keys 'vim .vimrc' C-m \; \
+  # Window 2 "Dev" / Pane 0
   select-pane -t 0 \; \
+  # Window 2 "Dev" / Pane 1
   split-window -v -p 30 \; \
   send-keys 'clear;python3' C-m \; \
+  # Window 2 "Dev" / Pane 2
   select-pane -t 2 \; \
+  # Window 3 "Bash" / Pane 0
   new-window -n Bash \; \
   split-window -h \; \
   split-window \; \
